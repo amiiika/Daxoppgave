18d5548adf13f4de74c32f14f69bb2a37a7072c4
Amy <amykaletabe@gmail.com>
2022-01-25 13:34:49 +0100

    FIxed local storage

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var names = [];
+const nameListKey = "key";
+
+let names = JSON.parse(localStorage.getItem(nameListKey));
+if (names == null) {
+  names = [];
+}
 
 //push and write out names
 function pushName() {
   let input = $("#name").val();
   names.push(input);
+  localStorage.setItem(nameListKey, JSON.stringify(names));
 
   $("#welcome").text(
     "Welcome, we wish you all the best " + names[names.length - 1] + "!"
@@ -12,12 +18,12 @@ function pushName() {
   $("#thanks").text("Thanks For Cheking Out My Website " + names.join(", "));
 }
 
-//make numbers
+//make numbers every min
+let randomNum = Math.floor(Math.random() * 9999);
+
 const luckyNumber = () => {
-  let randomNum = Math.floor(Math.random() * 9999);
   $("#number").text(randomNum);
 };
-
 
 //call functions
 luckyNumber();
