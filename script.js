a92b5731a2b693a08c3f92ae19b043fd4d3cc54a
Amy <amykaletabe@gmail.com>
2022-01-27 09:47:57 +0100

    Small visual update

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ if (names == null) {
 }
 
 //push and write out names
+
 function pushName() {
   let input = $("#name").val();
   names.push(input);
@@ -14,9 +15,10 @@ function pushName() {
   $("#welcome").text(
     "Welcome, we wish you all the best " + names[names.length - 1] + "!"
   );
-
   $("#thanks").text("Thanks For Cheking Out My Website " + names.join(", "));
 }
+$("#thanks").text("Thanks For Cheking Out My Website " + names.join(", "));
+
 
 //make numbers every min
 $("#number").text(Math.round(Math.random() * 1000));
