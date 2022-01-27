6705cac56987a2477959af67a65ef99d573bf007
Amy <amykaletabe@gmail.com>
2022-01-27 10:29:12 +0100

    Update number range

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ if (names == null) {
 }
 
 //enter = submit
-  $("body").keydown( function(e){
-    if(e.keyCode === 13){
-      e.preventDefault();
-      pushName()
-    }
-  })
+$("body").keydown(function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    pushName();
+  }
+});
 
 //push and write out names
 function pushName() {
@@ -19,7 +19,6 @@ function pushName() {
   names.push(input);
   localStorage.setItem(nameListKey, JSON.stringify(names));
   $("#name").val("");
-  
 
   $("#welcome").text(
     "Welcome, we wish you all the best " + names[names.length - 1] + "!"
@@ -28,13 +27,12 @@ function pushName() {
 }
 $("#thanks").text("Thanks For Cheking Out My Website " + names.join(", "));
 
-
 //make numbers every min
-$("#number").text(Math.round(Math.random() * 1000));
+$("#number").text(Math.round(Math.random() * 9999));
 
 setTimeout(() => {
   setInterval(() => {
-    $("#number").text(Math.round(Math.random() * 1000));
+    $("#number").text(Math.round(Math.random() * 9999));
   }, 60 * 1000);
 }, 60 - new Date().getSeconds() * 1000);
 
