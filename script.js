cbb56e4a68c3c2381cdcaf4022292aa1602d6205
Amy <amykaletabe@gmail.com>
2022-01-28 15:16:00 +0100

    semantic HTML

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const nameListKey = "key";
+const thanks = $("#thanks");
+const number = $("#number");
 
 let names = JSON.parse(localStorage.getItem(nameListKey));
 if (names == null) {
@@ -23,16 +25,16 @@ function pushName() {
   $("#welcome").text(
     "Welcome, we wish you all the best " + names[names.length - 1] + "!"
   );
-  $("#thanks").text("Thanks For Cheking Out My Website " + names.join(", "));
+  thanks.text("Thanks For Cheking Out My Website " + names.join(", "));
 }
-$("#thanks").text("Thanks For Cheking Out My Website " + names.join(", "));
+thanks.text("Thanks For Cheking Out My Website " + names.join(", "));
 
 //make numbers every min
-$("#number").text(Math.round(Math.random() * 9999));
+number.text(Math.round(Math.random() * 9999));
 
 setTimeout(() => {
-  $("#number").text(Math.round(Math.random() * 9999));
+  number.text(Math.round(Math.random() * 9999));
   setInterval(() => {
-    $("#number").text(Math.round(Math.random() * 9999));
+    number.text(Math.round(Math.random() * 9999));
   }, 60 * 1000);
 }, (60 - new Date().getSeconds()) * 1000);
